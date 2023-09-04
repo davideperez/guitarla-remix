af6762595d2dfdf3fd88f8b495694e6f1d5b980b
David <david.ezequiel.perez@gmail.com>
2023-09-04 16:13:59 -0300

    External Links added: a google font and normlizecss

--- app/root.jsx
+++ app/root.jsx
@@ -1,9 +1,12 @@
 import {
     Meta,
-    Links
+    Links,
+
 } from '@remix-run/react'
 
 import styles from './styles/index.css'
+
+
 
 export function meta() {
     return (
@@ -18,12 +21,29 @@ export function meta() {
 }
 
 export function links () {
+    //Order matters on the following:
     return [
         {
+            rel: "stylesheet",
+            href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+        },
+        {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com"
+        },
+        {
+            rel: 'preconnect',
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "true"
+        },
+        {
             rel: 'stylesheet',
+            href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap"
+        },
+        {
+            rel: "stylesheet",
             href: styles
-
-        }
+        },
     ]
 }
 
