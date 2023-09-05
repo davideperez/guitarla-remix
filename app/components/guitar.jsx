c7e60aeb9b9b499182725eee696aa76e93b86da7
David <david.ezequiel.perez@gmail.com>
2023-09-05 15:45:59 -0300

    Showing each guitar in the Store Component from the Guitar component.

--- app/components/guitar.jsx
+++ app/components/guitar.jsx
@@ -1,6 +1,19 @@
+import { Link } from "@remix-run/react"
+
 export default function Guitar ({guitar}) {
+
+  const { name, description, image, price, url } = guitar
+
+  let imageURL = image.data.attributes.formats.medium.url
   return (
-    <div>
+    <div className="guitar">
+      <img src={imageURL} alt={`${name} guitar`} />
+      <div className="container">
+        <h3>{name}</h3>
+        <p className="description">{description}</p>
+        <p className="price ">${price}</p>
+        <Link className="guitar-link" to={`/guitars/${url}`}>Product Details</Link>
+      </div>
     </div>
   )
 }
