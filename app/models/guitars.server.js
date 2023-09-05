df0cbfbbb653e98c7670b2e78585fb9152f31535
David <david.ezequiel.perez@gmail.com>
2023-09-05 18:32:32 -0300

    Getting a guitar by a filter on the strapi query to load the data into the guitarurl page.

--- app/models/guitars.server.js
+++ app/models/guitars.server.js
@@ -5,3 +5,12 @@ export async function getGuitars () {
 
     return results
 }
+
+export async function getGuitar(url) {
+    const endpoint_url = `${process.env.API_URL}/guitars?filters[url]=${url}&populate=image`
+    const answer = await fetch(endpoint_url)
+    const result = await answer.json()
+    console.log(result)
+
+    return result
+}
--- app/routes/guitars.$guitarURL.jsx
+++ app/routes/guitars.$guitarURL.jsx
@@ -1,11 +1,20 @@
+import { useLoaderData } from "@remix-run/react"
+import { getGuitar } from "../models/guitars.server"
+
 
 export async function loader({request, params}){
   const { guitarURL } = params
-  console.log(guitarURL)
-  return {}
+
+  const guitar = await getGuitar(guitarURL)
+
+  return guitar
 }
 
 export default function Guitar () {
+  const guitar = useLoaderData()
+  
+  console.log(guitar.data[0].attributes.name)
+  
   return (
     <div>$guitarurl</div>
   )
