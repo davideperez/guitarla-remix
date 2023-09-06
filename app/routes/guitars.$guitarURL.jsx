f9a5c79aff31b3514692f89a1215cea980af02b4
David <david.ezequiel.perez@gmail.com>
2023-09-06 11:23:51 -0300

    Showing the guitar information. Dynamic meta tags, added.

--- app/routes/guitars.$guitarURL.jsx
+++ app/routes/guitars.$guitarURL.jsx
@@ -1,6 +1,22 @@
 import { useLoaderData } from "@remix-run/react"
-import { getGuitar } from "../models/guitars.server"
+import { getGuitar } from "~/models/guitars.server"
+import styles from "~/styles/guitars.css"
 
+export function meta ({data}) {
+  return [
+    {title:`Guitar LA - ${data.data[0].attributes.name}`},
+    {description:`Guitars, guitar sales, ${data.data[0].attributes.name} guitar.`},
+  ]
+}
+
+export function links () {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader({request, params}){
   const { guitarURL } = params
@@ -12,10 +28,17 @@ export async function loader({request, params}){
 
 export default function Guitar () {
   const guitar = useLoaderData()
-  
-  console.log(guitar.data[0].attributes.name)
+
+  const { name, description, image, price } = guitar.data[0].attributes
   
   return (
-    <div>$guitarurl</div>
+    <main className="container guitar">
+      <img className='image' src={image.data.attributes.url} alt={`${name} "guitar"`} />
+      <div className="content">
+        <h3>{name}</h3>
+        <p className="text">{description}</p>
+        <p className="price">${price}</p>
+      </div>
+    </main>
   )
 }
