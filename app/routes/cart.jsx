de95587c50c7a6e09cf478cbb3bdd909f8bd3d3d
David <david.ezequiel.perez@gmail.com>
2023-09-11 14:27:57 -0300

    Cart component structure and css stylesheet, link and meta functions.

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -1,6 +1,31 @@
+import styles from '~/styles/cart.css'
+
+export const links = () => {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
+export const meta () => {}
+
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <main className="conainer">
+        <h1 className="heading">Shopping Cart</h1>
+        <div className="content">
+            <div className='cart'>
+                <h2>Items</h2>
+
+            </div>
+            <aside className="summary">
+                <h3>Summary</h3>
+                <p>Grand Total: $ </p>
+            </aside>
+        </div>
+    </main>
   )
 }
 
